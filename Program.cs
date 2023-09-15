db2a13d4b115faca65c8708ca465cb95f78e95f3
skari666 <144269531+skari666@users.noreply.github.com>
2023-09-15 13:49:39 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        int temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+
+        Console.WriteLine($"Теперь первое число равно {firstNumber}, а второе равно {secondNumber}");
+    }
+}
